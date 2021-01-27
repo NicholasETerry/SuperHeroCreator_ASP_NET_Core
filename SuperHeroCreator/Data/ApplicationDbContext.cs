ac4f091925a4f65ee113b8d8f14630bc584cdb1a
NicholasETerry <nicholas.e.terry@gmail.com>
2021-01-27 13:49:07 -0600

    1. created SuperHero controller outline 2. created superHeroAttributes Model 3. linked database to application

--- SuperHeroCreator/Data/ApplicationDbContext.cs
+++ SuperHeroCreator/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperHeroCreator.Models;
 
 namespace SuperHeroCreator.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<SuperHeroAttributes> SuperHero { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
